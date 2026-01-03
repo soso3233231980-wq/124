f0f23b264c0620aa6c12228a3132c4ce1e2bc394
soso3233231980-wq <soso3233231980@gmail.com>
2026-01-03 13:22:36 +0200

    Update f50.py

--- f50.py
+++ f50.py
@@ -10,7 +10,7 @@ ap.add_argument("-i", "--ip", required=True, type=str, help="Host ip")
 ap.add_argument("-p", "--port", required=True, type=int, help="Port")
 ap.add_argument("-c", "--choice", type=str, default="y", help="UDP(y/n)")
 ap.add_argument("-t", "--times", type=int, default=50000, help="Packets per one connection")
-ap.add_argument("-th", "--threads", type=int, default=1000, help="Threads")
+ap.add_argument("-th", "--threads", type=int, default=2000, help="Threads")
 args = vars(ap.parse_args())
 
 print("--> C0de By Lee0n123 <--")
